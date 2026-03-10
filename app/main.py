266af329341d1644e77c250191e610d12f5247f2
Mohamoudou <mohamoudou.sanogo@centrale-casablanca.ma>
2026-03-10 23:58:53 +0000

    ajout interface avec image
    Intégration du formulaire, ajout du dossier static avec logo et configuration des routes de base.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+from fastapi.staticfiles import StaticFiles
 
-@app.get("/")
-def read_root():
-    return {"message": "Bienvenue sur l'outil de diagnostic pédiatrique"}
+# Ajoute ceci après la ligne app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+# On explique à FastAPI où se trouve ton dossier de pages HTML
+templates = Jinja2Templates(directory="app/templates")
 
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    # Cette ligne dit : "Quand on va sur le site, affiche index.html"
+    return templates.TemplateResponse("index.html", {"request": request})
